0dd8f6c14e94c7e821bafb496b08a8041d93aa56
Juan A <avilajajuan@gmail.com>
2023-05-26 16:31:43 -0400

    created initialize, fill and move

--- practice-for-ch-ruby-first-tdd-long-practice-main/lib/hanoi.rb
+++ practice-for-ch-ruby-first-tdd-long-practice-main/lib/hanoi.rb
@@ -1,3 +1,26 @@
 class HanoiTowers
 
+    attr_reader :towers, :moves
+
+    def initialize
+
+        @towers = Array.new(3) {Array.new(3)}
+
+        @moves = 0
+
+    end 
+
+    def fill 
+
+        @towers[0] = [5,4,3,2,1]
+
+    end 
+
+    def move(start, end)
+
+
+
+    end 
+
+
 end
--- practice-for-ch-ruby-first-tdd-long-practice-main/spec/hanoi_spec.rb
+++ practice-for-ch-ruby-first-tdd-long-practice-main/spec/hanoi_spec.rb
@@ -5,8 +5,29 @@ describe HanoiTowers do
   subject(:htowers) { HanoiTowers.new() }
 
   describe "#initialize" do
+   
     it "create three towers as arrays" do
       expect(htowers.towers.length).to eq(3)
     end
-  end
-end
+
+    it 'sets moves equal to 0' do
+      expect(htowers.moves).to eq(0)
+    end
+
+  end 
+
+  describe "#fill" do 
+    it 'fills the first stack with five discs' do 
+      htowers.fill
+      expect(htowers.towers[0]).to eq([5,4,3,2,1])
+    end 
+  end 
+
+  describe '#move' do 
+    it 'places a disk in the appropriate tower' do 
+      htowers.move(0, 1)
+      expect(htowers.towers[0]).to eq([5,4,3,2])
+    end 
+  end 
+
+end 
